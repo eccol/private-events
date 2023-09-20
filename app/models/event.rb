c770896d7b7b21dadb635ceba3ec66070799326d
Eric <126840543+eccol@users.noreply.github.com>
2023-09-20 13:42:56 -0400

    Creator can delete events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :rsvps
+  has_many :rsvps, dependent: :destroy
   has_many :attendees, through: :rsvps, source: :user
   
   scope :past, -> { where('date < ?', Time.now)}
