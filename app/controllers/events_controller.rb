f1cc856d6e452e2724bcd8ff755f6161b74f5b9b
Eric <126840543+eccol@users.noreply.github.com>
2023-09-19 21:03:04 -0400

    Implement creating new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,27 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find_by(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title,:location,:date)
+  end
 end
