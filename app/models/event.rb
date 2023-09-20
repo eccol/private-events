8727b48314e1fdebf422c8ba7418e041807b307d
Eric <126840543+eccol@users.noreply.github.com>
2023-09-19 22:24:36 -0400

    Separate attended events into past and future

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :rsvps
   has_many :attendees, through: :rsvps, source: :user
+  
+  scope :past, -> { where('date < ?', Time.now)}
+  scope :future, -> { where('date > ?', Time.now)}
 end
