ee49df3fae62aefabf56a0075ff8210cc6bd3d74
Eric <126840543+eccol@users.noreply.github.com>
2023-09-22 23:06:46 -0400

    Fix registering with username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :users, only: [:show]
   resources :events do
     # post "/events/:event_id/rsvp", to: "rsvps#create"
