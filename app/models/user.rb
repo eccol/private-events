18941014d0b0057109686f1107efcda606987f28
Eric <126840543+eccol@users.noreply.github.com>
2023-09-22 22:52:16 -0400

    Add name to registration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, :email, presence: true
+
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   has_many :rsvps
   has_many :attended_events, through: :rsvps, source: :event
